3803ab95eecf2da22cf445dde30786fd038a7eca
SVishC <chaitanyamvishnu@gmail.com>
2018-07-03 14:02:08 +0530

    Deffered execution

--- Queries/MyLinq.cs
+++ Queries/MyLinq.cs
@@ -8,20 +8,36 @@ namespace Queries
 {
     public static class MyLinq
     {
+        //public static IEnumerable<T> Filter<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        //{
+        //    var result=new List<T>();
+
+        //    foreach (var item in source)
+        //    {
+        //        if (predicate(item))
+        //        {
+        //            result.Add(item);
+        //        }
+
+        //    }
+
+        //    return result;
+        //}
+
+        //For yield return
         public static IEnumerable<T> Filter<T>(this IEnumerable<T> source, Func<T, bool> predicate)
         {
-            var result=new List<T>();
+
 
             foreach (var item in source)
             {
                 if (predicate(item))
                 {
-                    result.Add(item);
+                    yield return item;
                 }
 
             }
 
-            return result;
         }
     }
 }
